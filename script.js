169f63f29e6117bc1647895a2449729c0b6957c0
Artyom Palevich <hello@apalevich.ru>
2022-08-15 18:28:18 +0300

    Layout cards

---
+++ script.js
@@ -0,0 +1,33 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        cards: [
+            {
+                name: 'Apple',
+                img: 'apple.jpg',
+            },
+            {
+                name: 'Banana',
+                img: 'banana.jpg',
+            },
+            {
+                name: 'Orange',
+                img: 'orange.jpg',
+            },
+            {
+                name: 'Pineapple',
+                img: 'pineapple.jpg',
+            },
+            {
+                name: 'Strawberry',
+                img: 'strawberry.jpg',
+            },
+            {
+                name: 'Watermelon',
+                img: 'watermelon.jpg',
+            },
+        ]
+    }
+});
+
+console.log(app);
